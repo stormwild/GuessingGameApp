01e8b352ce05e64acc9c3bdf6cf758047bdb474d
Alexander Torrijos <artorrijos@gmail.com>
2021-01-10 18:50:27 +0800

    Updates

--- app/src/main/java/com/stormwild/guessinggame/MainActivity.java
+++ app/src/main/java/com/stormwild/guessinggame/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 checkGuess();
+            }
+        });
+
+        guessText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                checkGuess();
+                return true;
             }
         });
 
